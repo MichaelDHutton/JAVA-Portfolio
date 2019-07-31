4c1aaa2740ba4c1ac6fe9d3830851770c277948c
MichaelDHutton <52721083+MichaelDHutton@users.noreply.github.com>
2019-07-31 17:23:28 -0500

    Add files via upload

---
+++ welcome2/Welcome2.bat
@@ -0,0 +1,4 @@
+echo off
+"C:\Program Files\Java\jdk1.8.0_101\bin"\javac Welcome2.java
+"C:\Program Files\Java\jdk1.8.0_101\bin"\java Welcome2
+pause
---
+++ welcome2/Welcome2.java
@@ -0,0 +1,9 @@
+
+public class Welcome2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Welcome to ");
+		System.out.println("Java Programming!");
+	}
+}
