30fff27ab4ac9b6ccaeaa47da02d0c5fadcaa85b
MichaelDHutton <52721083+MichaelDHutton@users.noreply.github.com>
2019-08-01 14:21:27 -0500

    Add files via upload

---
+++ Bank/AccountTest.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class AccountTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		Account myAccount= new Account();
+		
+		System.out.printf("Initial name is : %s%n%n", myAccount.getName());
+		
+		System.out.println("Please enter the name: ");
+		String theName = input.nextLine();
+		myAccount.setName(theName);
+		
+		System.out.println();
+		
+		System.out.printf("Name in object MyAccount is : %n%s%n", myAccount.getName());
+	}
+
+}
---
+++ Bank/account.bat
@@ -0,0 +1,4 @@
+echo off
+"C:\Program Files\Java\jdk1.8.0_101\bin"\javac AccountTest.java
+"C:\Program Files\Java\jdk1.8.0_101\bin"\java AccountTest
+pause
