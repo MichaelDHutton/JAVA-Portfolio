d434bd31cd15c37701525b3ce047c66e015393f3
MichaelDHutton <52721083+MichaelDHutton@users.noreply.github.com>
2019-08-01 10:37:21 -0500

    Add files via upload

---
+++ welcome3/welcome3.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+
+/**
+ * @author Michael Hutton
+ *
+ */
+public class welcome3 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+			System.out.println("Welcome\nto\nJava\nProgramming");
+	}
+
+}
