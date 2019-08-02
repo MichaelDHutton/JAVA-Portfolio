3294380d5b7460b8287b9cef2c10dd93e896d9e8
MichaelDHutton <52721083+MichaelDHutton@users.noreply.github.com>
2019-08-02 14:24:19 -0500

    added account balance

--- Bank/Account.java
+++ Bank/Account.java
@@ -1,13 +1,34 @@
 
 public class Account 
 {
-	private String name; //instance variable
+	private String name; //instance variable name
+	private double balance; // instance variable balance 
 	
 	//constructor initializes name with parameter name
-	public Account(String name)// constructor name is class name 
+	public Account(String name, double balance)// constructor name is class name 
 	{
 		this.name = name;
+		
+		// validate that the balance is greater than 0.0; if it's not,
+		//instance variable balance keeps its default initial variable balance
+		if (balance > 0.0) 
+		{
+			this.balance = balance;
+			}
 	}
+	
+	public void deposit(double depositAmount) 
+	{
+		if (depositAmount > 0.0) 
+		{
+			balance = balance + depositAmount;
+			}
+		}
+public double getBalance() 
+{
+return balance;	
+}
+	
 	
 	//method to set the name 
 	public void setName(String name) 
--- Bank/AccountTest.java
+++ Bank/AccountTest.java
@@ -1,18 +1,45 @@
+import java.util.Scanner;
+
 
 
 public class AccountTest {
 
 	public static void main(String[] args) 
 	{ 
-		// TODO Auto-generated method stub
 		
 		
-		Account myAccount1 = new Account("Michael Hutton");
-		Account myAccount2 = new Account("Mike Hutton");
+		Account account1 = new Account("Michael Hutton", 50.00);
+		Account account2 = new Account("Mike Hutton", -7.53);
 		
-		System.out.printf("Initial name is : %s%n%n", myAccount1.getName());
-		System.out.printf("Initial name is : %s%n%n", myAccount2.getName());
+		System.out.printf("%s  balance: $%.2f%n", 
+				account1.getName(), account1.getBalance());
+		System.out.printf("%s  balance: $%.2f%n", 
+				account2.getName(), account2.getBalance());
 		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.print("Enter deposit amount for account1: ");
+		double depositAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account1 balance%n%n", 
+				depositAmount);
+		account1.deposit(depositAmount);
+		
+		System.out.printf("%s balance: $%.2f%n",
+				account1.getName(),	account1.getBalance());
+		System.out.printf("%s balance: $%.2f%n",
+				account2.getName(), account2.getBalance());
+		
+		System.out.print("Enter deposit amount for account2: ");
+		depositAmount = input.nextDouble();
+		System.out.printf("%nadding %.2f to account2 balance%n%n", 
+				depositAmount);
+		account2.deposit(depositAmount);
+		
+		
+		System.out.printf("%s  balance: $%.2f%n", 
+				account1.getName(), account1.getBalance());
+		System.out.printf("%s  balance: $%.2f%n", 
+				account2.getName(), account2.getBalance());
 	}
 
 }
