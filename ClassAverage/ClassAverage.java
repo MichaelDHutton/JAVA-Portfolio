209ab468e0aae4596d1e194d89d5de96ee811672
Michael Hutton <Michael.dwayne.Hutton@gmail.com>
2019-08-04 17:49:39 -0500

    updated
    Now the program uses a while loop to allow the user to enter unlimited grades and has an exit generating the total number of grades entered and the class average.

--- ClassAverage/ClassAverage.java
+++ ClassAverage/ClassAverage.java
@@ -21,20 +21,32 @@ public class ClassAverage {
 			
 			
 			int total = 0;
-			int gradeCounter = 1;
+			int gradeCounter = 0;
 			
-			while (gradeCounter  <= 10) 
+			System.out.print("Enter grade or -1 to quit: ");
+			int grade = input.nextInt();
+			
+			while (grade != -1) 
 			{
-				System.out.print("Enter grade: ");
-				int grade = input.nextInt();
 				total = total + grade;
 				gradeCounter = gradeCounter + 1;
+				
+				System.out.print("Enter grade or -1 to quit: ");
+				grade = input.nextInt();
+				
 			}
 			
-			int average = total / 10;
-			
-			System.out.printf("%nTotal of all 10 grades is %d%n", total);
-			System.out.printf("%nClass average is %d%n", average);
+			if (gradeCounter != 0) 
+			{
+				double average = (double) total / gradeCounter;
+				System.out.printf("%nTotal of the %d grades entered is %d%n", gradeCounter, total);
+				System.out.printf("Class average is %.2f%n", average);
+			}
+			else 
+			{
+			System.out.println("No grades were entered");
+			}
 	}
-
-}
+	}
+			
+			
