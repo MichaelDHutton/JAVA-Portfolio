acaad0218329f49d26da138a9e4a2ad6ccd17194
Michael Hutton <Michael.dwayne.Hutton@gmail.com>
2019-08-07 18:25:39 -0500

    Add files via upload

---
+++ MethodOverload/MethodOverload.bat
@@ -0,0 +1,4 @@
+echo off
+"C:\Program Files\Java\jdk1.8.0_101\bin"\javac MethodOverload.java
+"C:\Program Files\Java\jdk1.8.0_101\bin"\java MethodOverload
+pause
---
+++ MethodOverload/MethodOverload.java
@@ -0,0 +1,30 @@
+/**
+ * 
+ */
+
+/**
+ * @author Michael Hutton
+ *
+ */
+public class MethodOverload {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.printf("Square of integer 7 is %d%n", square(7));
+		System.out.printf("Square of double 7 is %f%n", square(7.5));
+	}
+		public static int square(int intValue) 
+		{
+			System.out.printf("%nCalled square with int argument: %d%n", intValue);
+			return intValue * intValue;
+		}
+		
+		public static double square(double doubleValue) 
+		{
+			System.out.printf("%nCalled square with double argument: %f%n", doubleValue);
+			return doubleValue * doubleValue;
+		}
+}
