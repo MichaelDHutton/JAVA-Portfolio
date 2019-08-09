2de34e56ce67084fdd445a8049fd3301382bc6f3
Michael Hutton <Michael.dwayne.Hutton@gmail.com>
2019-08-09 15:15:47 -0500

    Add files via upload

--- GradeBook/GradeBook.java
+++ GradeBook/GradeBook.java
@@ -8,9 +8,9 @@
  */
 public class GradeBook {
 	private String courseName;
-	private int[] grades;
+	private int[][] grades;
 	
-	public GradeBook( String courseName, int[] grades) 
+	public GradeBook( String courseName, int[] [] grades) 
 		{
 		this.courseName  = courseName;
 		this.grades = grades;
@@ -31,63 +31,70 @@ public class GradeBook {
 		{
 		 outputGrades();
 		 
-		 System.out.printf("%nClass average is %.2f%n", getAverage());
-		 
-		 System.out.printf("Lowest grade is %d%nHighest grade is %d%n%n", 
-				 getMinimum(), getMaximum());
+		 System.out.printf("%n%s %d%n%s %d%n%n",
+				 "Lowest grade in the grade book is", getMinimum(),
+				 "Highest grade in the grade book is", getMaximum());
 		 
 		 outputBarChart();
-	 }
-	  
+		}
+			  
 		public int getMinimum() 
 		{
-		  int lowGrade = grades[0];
+		  int lowGrade = grades[0][0];
 		  
-		  for (int grade : grades) 
+		  for (int [] studentGrades : grades) 
 		  {
-			  if (grade < lowGrade) 
+			  for (int grade : studentGrades) 
 			  {
+			  if (grade < lowGrade) 
+			  	{
 				  lowGrade = grade;
-			  }
-		  }
+			  	}
+		      }
+	      }
 		  return lowGrade;
 	  }
 	  
 		public int getMaximum() 
 	  {
-		  int highGrade = grades[0];
+		  int highGrade = grades[0][0];
 		  
-		  for (int grade : grades) 
+		  for (int [] studentGrades : grades) 
 		  {
-			  if (grade > highGrade) 
-			  {
-				  highGrade = grade;
-			  }
+			  for (int grade : studentGrades) 
+			  	{
+				  if (grade > highGrade) 
+				  { 
+					  highGrade = grade;
+			  	  }
+			  	}
 		  }
 		  return highGrade;
 	  }
 	  
-	  public double getAverage() 
+	  public double getAverage(int[] setOfGrades) 
 	  {
 		int total = 0;
 		
-		for (int grade : grades) 
+		for (int grade : setOfGrades) 
 		{
 			total += grade;
 			}
-		return (double) total / grades.length;
+		return (double) total / setOfGrades.length;
 	  }
 	
 	  public void outputBarChart() 
 	  {
-		  System.out.println("Grade distribution: ");
+		  System.out.println("Overall Grade distribution: ");
 		  
 		  int[] frequency = new int[11];
 		  
-		  for (int grade : grades) 
+		  for (int [] studentGrades : grades) 
 		  {
+			  for (int grade : studentGrades) {
 			  ++frequency[grade / 10];
 		  }
+	  }
 		  for (int count = 0; count < frequency.length; count++) 
 		  {
 			  if (count == 10) 
@@ -106,11 +113,24 @@ public class GradeBook {
 	  public void outputGrades() 
 	  {
 		  System.out.printf("The Grades are :%n%n");
+		  System.out.print("                 ");
 		  
-		  for (int student = 0; student < grades.length; student++) {
-		  System.out.printf("Student  %2d:  %3d%n", 
-				  student + 1, grades[student]);
+		  for(int test = 0; test < grades[0].length; test++) 
+		  {
+			  System.out.printf("Test  %d  ", test +1);
 		  }
-		
+		  System.out.println("Average");
+	  		
+		  for (int student = 0; student < grades.length; student++) {
+		  System.out.printf("Student  %2d", student +1);
+		  
+		  for (int test : grades[student]) {
+			  System.out.printf("%8d", test);
+		  
+		  }  
+			double average = getAverage(grades[student]);
+			System.out.printf("%9.2f%n", average);  
+		  }
 	  }
-	}
+}
+	 
--- GradeBook/GradeBookRunner.java
+++ GradeBook/GradeBookRunner.java
@@ -13,10 +13,22 @@ public class GradeBookRunner {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-			int[] gradesArray = {87, 98, 52, 89, 65, 2, 78, 87, 45, 66};
+			int[][] gradesArray = {{87, 98, 52},
+								   {89, 65, 2},
+								   {78, 87, 45},
+								   {66, 98, 58},
+								   {56, 98, 100},
+								   {89, 98, 78},
+								   {89, 69, 89},
+								   {69, 89, 98},
+								   {97, 99, 86},
+								   {100, 98, 78}};
 			
-			GradeBook myGradeBook = new GradeBook("Java Programming 101", gradesArray);
-			System.out.printf("Welcome to the grade book for%n%s%n%n", myGradeBook.getCourseName());
+			
+			GradeBook myGradeBook = new GradeBook(
+					"Java Programming 101", gradesArray);
+			System.out.printf("Welcome to the grade book for%n%s%n%n", 
+					myGradeBook.getCourseName());
 			myGradeBook.processGrades();
 	}
 
